b0b6760e17b856fe9676cd71211c015f3f9618b2
KoocieBoi <>
2018-04-27 21:58:59 +0300

    Removed failed attempt on Random Cat command

--- Files/Commands/cat.js
+++ Files/Commands/cat.js
@@ -4,12 +4,6 @@ const axios = require("axios");
 module.exports = (Message, Arguments) => {
     if (Arguments[0] !== undefined) EmbedsFile.SendCommandWrongUsage(Message, 4);
     else {
-        axios.get("http://thecatapi.com/api/images/get?format=xml")
-            .then((Cat) => {
-                let FormatImageURL = /\\+/g;
-                let CatImage = Cat.data.file
-                EmbedsFile.SendCatCommandMessage(Message, Result);
-            });
         axios.get("http://aws.random.cat/meow")
             .then((Cat) => {
                 let FormatImageURL = /\\+/g;
