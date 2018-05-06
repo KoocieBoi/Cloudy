65fe81a9ef415a7a09dc24defd2fc268008f6680
KoocieBoi <>
2018-05-06 16:03:38 +0300

    Fixed config file

--- Files/configuration.js
+++ Files/configuration.js
@@ -1,15 +1,18 @@
-exports = {
-    bot: {
-        token: process.env.TOKEN,
-        id: "427799231399591946",
-        letterPrefix: "cl!",
-        mentionprefix: function () { return `<@${this.id}> `; }
-    },
-    guild: {
-    
-    },
-    embeds: { color: 0x3f51b5 },
-    commands: {
+exports.bot = {
+    token: process.env.TOKEN,
+    id: "427799231399591946",
+    letterPrefix: "cl!",
+    mentionprefix: function () { return `<@${this.id}> `; } 
+};
 
-    }
+exports.guild = {
+
+};
+
+exports.embeds = {
+    color: 0x3f51b5
+};
+
+exports.commands = {
+
 };
