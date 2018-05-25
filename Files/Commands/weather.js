b6ff37d82cb5b8397ea5e7ec52618a380c5f0233
KoocieBoi <>
2018-05-25 23:03:42 +0300

    trying to fix

--- Files/Commands/weather.js
+++ Files/Commands/weather.js
@@ -11,7 +11,8 @@ module.exports = (msg, args, client) => {
         }, (err, result) => {
             if (err) Embeds.SendErrorWebhook(msg, client, err, "cl_weather");
             else {
-                let data = JSON.stringify(result, null, 2);
+                let data = JSON.parse(JSON.stringify(result, null, 2));
+                
                 Embeds.SendWeatherCommandMessage(msg, data);
             } 
         });
