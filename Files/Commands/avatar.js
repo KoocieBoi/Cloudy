d4db7c9f8655f6a61f7ce529508fa129555c483d
KoocieBoi <26856087+KoocieBoi@users.noreply.github.com>
2018-06-25 20:21:48 +0300

    Luck at 4th try?

--- Files/Commands/avatar.js
+++ Files/Commands/avatar.js
@@ -3,13 +3,14 @@ let Embeds = require("../embeds");
 module.exports = (msg, args) => {
     function returnArgsIsMention() {
         if (args[0] && msg.mentions && msg.mentions.members && args[0] === msg.mentions.members.first().toString()) return true;
+        else if (!args[0]) return true;
         else return false;
     }
 
     let finalUser;
-    if (returnArgsIsMention()) finalUser = msg.mentions.members.first().user;
-    else finalUser = msg.author;
-
+    if (returnArgsIsMention() && args[0]) finalUser = msg.mentions.members.first().user;
+    else if (returnArgsIsMention() && !args[0]) finalUser = msg.author;
+ 
     if (args[1] || !returnArgsIsMention()) Embeds.SendCommandWrongUsage(msg, 12);
     else Embeds.SendAvatarCommandMessage(msg, finalUser);
 
