bd8715b4a70ea8a6339a43f4e0e3577720878cde
KoocieBoi <26856087+KoocieBoi@users.noreply.github.com>
2018-06-25 20:12:16 +0300

    Possible fix no. 3

--- Files/Commands/avatar.js
+++ Files/Commands/avatar.js
@@ -1,17 +1,14 @@
 let Embeds = require("../embeds");
 
 module.exports = (msg, args) => {
-    let author = msg.author;
-    let mention = msg.mentions;
-
     function returnArgsIsMention() {
-        if (args[0] && mention && args[0] === mention.members.first().toString()) return true;
+        if (args[0] && msg.mentions && msg.mentions.members && args[0] === msg.mentions.members.first().toString()) return true;
         else return false;
     }
 
     let finalUser;
-    if (returnArgsIsMention()) finalUser = mention.members.first();
-    else finalUser = author;
+    if (returnArgsIsMention()) finalUser = msg.mentions.members.first().user;
+    else finalUser = msg.author;
 
     if (args[1] || !returnArgsIsMention()) Embeds.SendCommandWrongUsage(msg, 12);
     else Embeds.SendAvatarCommandMessage(msg, finalUser);
