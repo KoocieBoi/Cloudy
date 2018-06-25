ee0bc17ecf400dd7fd9363d12ffe06e8b550b98e
KoocieBoi <26856087+KoocieBoi@users.noreply.github.com>
2018-06-25 19:36:09 +0300

    Possible fix for the cl_avatar command

--- Files/Commands/avatar.js
+++ Files/Commands/avatar.js
@@ -8,18 +8,20 @@ module.exports = (msg, args) => {
 
     function returnArgsIsMention() {
         if (args[0]) {
-            if (args[0] === user.mention.toString()) return true;
+            if (user.mention) {
+                if (args[0] === user.mention.toString()) return true;
+                else return false;
+            }
             else return false;
         }
         else return true;
     }
 
     let finalUser;
-    if (user.mention) finalUser = user.mention.user;
+    if (returnArgsIsMention()) finalUser = user.mention.user;
     else finalUser = user.author;
 
     if (args[1] || !returnArgsIsMention()) Embeds.SendCommandWrongUsage(msg, 12);
-    else if (args[0] === user.mention.toString()) Embeds.SendAvatarCommandMessage(msg, finalUser);
-    else if (!args[0]) Embeds.SendAvatarCommandMessage(msg, user.author);  
+    else Embeds.SendAvatarCommandMessage(msg, finalUser);
 
 };
