3bf2b0992fa1be619ce4d1711a2f04738a88c9df
KoocieBoi <26856087+KoocieBoi@users.noreply.github.com>
2018-06-27 21:35:12 +0300

    Fix for the command handler

--- Files/Events/message.js
+++ Files/Events/message.js
@@ -20,7 +20,7 @@ module.exports = (msg, client) => {
 	}
 
 	// Command Handler
-	fs.readdir("../Commands", (err, files) => {
+	fs.readdir("../Commands/", (err, files) => {
 		if (err) {
 			console.log(err);
 			return;
